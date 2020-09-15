4ce0a3a404868b57903659d682403b5d78dd2b6b
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2020-09-15 11:49:39 +0200

    Dodaj pliki projektów.

---
+++ ProjectManager.Blazor.Server/BlazorApplication.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace ProjectManager.Blazor.Server {
+    partial class ProjectManagerBlazorApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Blazor.SystemModule.SystemBlazorModule();
+            this.module3 = new ProjectManager.Module.ProjectManagerModule();
+            this.module4 = new ProjectManager.Module.Blazor.ProjectManagerBlazorModule();
+            this.objectsModule = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // ProjectManagerBlazorApplication
+            // 
+            this.ApplicationName = "ProjectManager";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.objectsModule);
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.ProjectManagerBlazorApplication_DatabaseVersionMismatch);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Blazor.SystemModule.SystemBlazorModule module2;
+        private ProjectManager.Module.ProjectManagerModule module3;
+        private ProjectManager.Module.Blazor.ProjectManagerBlazorModule module4;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule objectsModule;
+    }
+}
